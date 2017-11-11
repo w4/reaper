511f1312af2f01eaf24b92ed65629bccfe487144
Jordan Doyle <jordan@9t9t9.com>
2017-11-11 11:51:35 +0000

    Complete rewrite using Rust

--- README.md
+++ README.md
@@ -1,17 +1,42 @@
 # reaper
 
-[![License](https://poser.pugx.org/laravel/framework/license.svg)](http://github.com/jordandoyle/reaper)
+[![License](https://img.shields.io/github/license/w4/reaper.svg?style=flat-square)](http://github.com/jordandoyle/reaper)
 
-[League of Legends](http://leagueoflegends.com) mass summoner name checker. Supply a region and a list and the script will check the list for available summoner names. Common uses are finding quick variations of your name or finding rare (or "OG") names for selling. An API Key is required to do use this script, they are available for free from [Riot Games](https://developer.riotgames.com/), the API key is set in **reaper.js**.
+[League of Legends](http://leagueoflegends.com) mass summoner name checker. Supply a region and a list
+and the application will check the list for available summoner names. Common uses are finding quick
+variations of your name or finding rare (or "OG") names for selling. An API Key is required to do use
+this script, they are available for free from [Riot Games](https://developer.riotgames.com/).
 
-You can find a list of the servers you can query from on [Riot's website](https://developer.riotgames.com/regional-endpoints.html).
+You can find a list of the servers you can query from on
+[Riot's website](https://developer.riotgames.com/regional-endpoints.html). Examples of inputs for
+SERVER: `euw1`, `na1`, `pbe1`.
 
-The syntax of reaper is very simple:
+    Reaper 0.1.0
+    Jordan Doyle <jordan@9t9t9.com>
+    Scans over a given list for available usernames on League of Legends
 
-    node reaper.js [server (na1/euw1/la1/etc)] [username file] (output file)
+    USAGE:
+        reaper [FLAGS] [OPTIONS] <SERVER> <INPUT> <API KEY>
+
+    FLAGS:
+        -h, --help       Prints help information
+        -V, --version    Prints version information
+        -v, --verbose    Increases logging verbosity each use up to 3 times
+
+    OPTIONS:
+        -o, --output <FILE>    Sets an output file to write available usernames to
+
+    ARGS:
+        <SERVER>     Sets the server to search for usernames on
+        <INPUT>      Sets the input file to use
+        <API KEY>    Sets the API key to use
 
 For example:
 
-    node reaper.js euw1 username_list.txt output.txt
+    ./reaper euw1 username_list.txt my-api-key -o output.txt
 
-Will check the list username_list.txt for available summoner names on Europe West and output what it finds to output.txt
+Will check the list `username_list.txt` for available summoner names on Europe West using API key `my-api-key` and
+outputs what it finds to output.txt
+
+Builds are available under releases or you can build it from source by pulling down the code and running
+`cargo build --release`.
---
+++ src/main.rs
@@ -0,0 +1,175 @@
+extern crate chrono;
+#[macro_use]
+extern crate clap;
+extern crate fern;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate log;
+extern crate ratelimit;
+extern crate regex;
+extern crate reqwest;
+
+use std::time::Duration;
+use std::error::Error;
+use std::io::{BufRead, BufReader, Write};
+use std::fs::{File, OpenOptions};
+use std::path::Path;
+use clap::{App, Arg};
+use reqwest::StatusCode;
+use regex::Regex;
+
+lazy_static! {
+    static ref CLIENT: reqwest::Client = reqwest::Client::new();
+    static ref USERNAME_REGEX: Regex = Regex::new(r"^[0-9\p{L} _\\.]{3,16}$").unwrap();
+}
+
+fn setup_logger(verbosity: u64) -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{} [{}] [{}] {}",
+                chrono::Local::now().format("%e %b %Y %H:%M:%S%.3f"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(match verbosity {
+            0 | 1 => log::LogLevelFilter::Info,
+            2 => log::LogLevelFilter::Debug,
+            _ => log::LogLevelFilter::Trace,
+        })
+        .level_for("reaper", match verbosity {
+            0 => log::LogLevelFilter::Info,
+            1 => log::LogLevelFilter::Debug,
+            _ => log::LogLevelFilter::Trace,
+        })
+        .chain(std::io::stdout())
+        .apply()?;
+
+    Ok(())
+}
+
+fn argparse<'a, 'b>() -> clap::App<'a, 'b> {
+    App::new("Reaper")
+        .version(crate_version!())
+        .author("Jordan Doyle <jordan@9t9t9.com>")
+        .about("Scans over a given list for available usernames on League of Legends")
+        .arg(Arg::with_name("SERVER")
+                .help("Sets the server to search for usernames on")
+                .required(true)
+                .index(1))
+        .arg(Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true)
+                .index(2))
+        .arg(Arg::with_name("API KEY")
+                .help("Sets the API key to use")
+                .required(true)
+                .index(3))
+        .arg(Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .value_name("FILE")
+                .help("Sets an output file to write available usernames to")
+                .takes_value(true))
+        .arg(Arg::with_name("verbose")
+                .short("v")
+                .long("verbose")
+                .multiple(true)
+                .help("Increases logging verbosity each use up to 3 times"))
+}
+
+fn main() {
+    let args = argparse().get_matches();
+
+    setup_logger(args.occurrences_of("verbose")).expect("Failed to initialize logging.");
+
+    info!("Reaper booting up.");
+
+    // all our inputs below are required fields so we can unwrap them without any worries
+    let server = args.value_of("SERVER").unwrap();
+    let input = Path::new(args.value_of("INPUT").unwrap());
+    let api_key = args.value_of("API KEY").unwrap();
+
+    assert!(input.exists(), "Input file doesn't exist.");
+
+    let output = args.value_of("output");
+
+    // output is an optional field so we check if it has a value and if it does we build a
+    // new File instance and create a new Option with it
+    let output_file = if output.is_some() {
+        match OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open(output.unwrap())
+        {
+            Ok(file) => Some(file),
+            Err(e) => {
+                error!("Couldn't open handle to output file. {}", e);
+                panic!()
+            }
+        }
+    } else {
+        None
+    };
+
+    debug!("Finished parsing arguments");
+
+    let mut ratelimit = ratelimit::Builder::new()
+        .capacity(1) // number of tokens the bucket will hold
+        .quantum(1) // add one token per interval
+        .interval(Duration::new(1, 0)) // add quantum tokens every 1 second
+        .build();
+
+    for line in BufReader::new(File::open(input).unwrap()).lines() {
+        let username = &line.unwrap();
+
+        if !USERNAME_REGEX.is_match(username) {
+            error!(
+                "The username \"{}\" isn't a valid username, skipping.",
+                username
+            );
+            continue;
+        }
+
+        debug!("Checking if \"{}\" is available", username);
+
+        match send_request(server, api_key, username) {
+            Ok(_) => {
+                info!("{} is available!", username);
+
+                if let Some(ref mut file) = output_file.as_ref() {
+                    writeln!(file, "{}", username).unwrap();
+                }
+            }
+            Err(e) => info!("{} is not available ({})", username, e),
+        }
+
+        ratelimit.wait();
+    }
+}
+
+fn send_request(server: &str, api_key: &str, username: &str) -> Result<(), Box<Error>> {
+    let resp = CLIENT
+        .get(&format!(
+            "https://{}.api.riotgames.com/lol/summoner/v3/summoners/by-name/{}?api_key={}",
+            server,
+            username,
+            api_key
+        ))
+        .send()?;
+
+    debug!("{:?}", resp);
+
+    if !resp.status().is_success() {
+        if resp.status().eq(&StatusCode::NotFound) {
+            Ok(())
+        } else {
+            Err(Box::from("Bad response from Riot API"))
+        }
+    } else {
+        Err(Box::from("Username is taken"))
+    }
+}
